1bf9eff4da0de6b9a1d81c467abeeb83eaf9d3b0
1yacin <93822862+1yacin@users.noreply.github.com>
2025-02-01 12:26:43 +0100

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ module.exports = {
   },
   INTERACTIONS: {
     SLASH: true, // Should the interactions be enabled
-    CONTEXT: false, // Should contexts be enabled
-    GLOBAL: false, // Should the interactions be registered globally
+    CONTEXT: true, // Should contexts be enabled
+    GLOBAL: true, // Should the interactions be registered globally
     TEST_GUILD_ID: "914277797449568306", // Guild ID where the interactions should be registered. [** Test you commands here first **]
   },
   EMBED_COLORS: {
@@ -19,7 +19,7 @@ module.exports = {
     WARNING: "#F7E919",
   },
   CACHE_SIZE: {
-    GUILDS: 100,
+    GUILDS: 1,
     USERS: 10000,
     MEMBERS: 10000,
   },
